b938ca2e938d3d23d2eb7deee54b82b414ea1efd
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-01-03 19:13:07 +0300

    Enhance user registration and login by adding hidden password input and Base64 encoding for password storage

--- auth.cpp
+++ auth.cpp
@@ -1,51 +1,129 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 #include "auth.h"
-
+#include <termios.h>
+#include <unistd.h>
 
 using namespace std;
 
+// Function to get hidden password input
+string getHiddenPassword() {
+    string password;
+    char ch;
+    struct termios oldt, newt;
+
+    // Turn off terminal echo
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~ECHO; // Disable echoing characters
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    // Read password one character at a time
+    while ((ch = getchar()) != '\n' && ch != EOF) {
+        password += ch;
+    }
+
+    // Restore terminal settings
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    cout << endl; // Move to a new line after input
+
+    return password;
+}
+
+
+// Function to encode a string in Base64
+string base64Encode(const string& data) {
+    const string base64_chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789+/";
+
+    string encoded;
+    int val = 0, valb = -6;
+
+    for (unsigned char c : data) {
+        val = (val << 8) + c;
+        valb += 8;
+        while (valb >= 0) {
+            encoded.push_back(base64_chars[(val >> valb) & 0x3F]);
+            valb -= 6;
+        }
+    }
+    if (valb > -6) encoded.push_back(base64_chars[((val << 8) >> (valb + 8)) & 0x3F]);
+    while (encoded.size() % 4) encoded.push_back('=');
+    return encoded;
+}
+// Function to get hidden password input
+// string getHiddenPassword() {
+//     string password;
+//     char ch;
+//     struct termios oldt, newt;
+
+//     // Turn off terminal echo
+//     tcgetattr(STDIN_FILENO, &oldt);
+//     newt = oldt;
+//     newt.c_lflag &= ~ECHO;
+//     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+//     // Read password
+//     while ((ch = getchar()) != '\n' && ch != EOF) {
+//         password += ch;
+//     }
+
+//     // Restore terminal echo
+//     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+//     cout << endl;
+
+//     return password;
+// }
+
+// Function to register a new user
 void registerUser() {
+    string username, password;
 
-  string username, password;
+    cout << "Register a new account\n";
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter password: ";
+    password = getHiddenPassword(); // Use hidden password input
 
-  cout << "Register a new account" << endl;
-  cout << "Enter a username: ";
-  cin >> username;
-  cout << "Enter a password: ";
-  cin >> password;
+    string encodedPassword = base64Encode(password); // Encode password
 
-  ofstream file("users.txt", ios::app);
-  if (file.is_open()) {
-    file << username << " " << password << endl;
-    file.close();
-    cout << "Account created successfully" << endl;
-  } else {
-    cout << "Error: Unable to open file" << endl;
-  }
+    // Open the file in append mode to add new credentials
+    ofstream file("users.txt", ios::app);
+    if (file.is_open()) {
+        file << username << " " << encodedPassword << "\n";
+        file.close();
+        cout << "Registration successful! You can now log in.\n";
+    } else {
+        cout << "Error: Unable to open file for writing.\n";
+    }
 }
 
+// Function to log in a user
 bool loginUser() {
+    string username, password;
 
-  string username, password, u, p;
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter password: ";
+    password = getHiddenPassword(); // Use hidden password input
 
-  cout << "Login to your account" << endl;
-  cout << "Enter your username: ";
-  cin >> username;
-  cout << "Enter your password: ";
-  cin >> password;
-
-  return validateCredentials(username, password);
+    return validateCredentials(username, password);
 }
 
+// Function to validate user credentials against stored data
 bool validateCredentials(const string& username, const string& password) {
     string storedUsername, storedPassword;
+    string encodedPassword = base64Encode(password); // Encode entered password for comparison
 
     // Open the file in read mode to verify credentials
     ifstream file("users.txt");
     if (file.is_open()) {
         while (file >> storedUsername >> storedPassword) {
-            if (storedUsername == username && storedPassword == password) {
+            if (storedUsername == username && storedPassword == encodedPassword) {
                 file.close();
                 return true; // Match found
             }
